bd9c838ba4e70ec9d84db6fa80f12af1df10432c
Thomas Ruppeiner <truppeiner@gmail.com>
2022-07-15 14:06:33 -0500

    first question functioning

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+let timer = document.querySelector('.timer-clock');
+
+let startQuiz = function() {
+    let firstQuestion = function() {
+        let quizQuestion = document.querySelector(".quiz-question");
+            quizQuestion.textContent="What language creates the basic layout of any website?"
+            document.querySelector('.start-section').hidden = true;
+
+        let questionOneA = document.querySelector('.option-1');
+            let answerOneA = document.createElement("button");
+                answerOneA.className = "btn btn-success m-3 answer-1 w-50";
+                answerOneA.textContent = "A. HTML";
+            questionOneA.appendChild(answerOneA);
+            // pass to second question
+            questionOneA.addEventListener('click', secondQuestion);
+
+
+        let questionOneB = document.querySelector('.option-2'); 
+            let answerOneB = document.createElement("button");
+                answerOneB.className = "btn btn-success m-3 answer-2 w-50";
+                answerOneB.textContent = "B. Python";
+            questionOneB.appendChild(answerOneB);
+            // check to see if correct answer is written is sellected
+            questionOneB.addEventListener('click', checkB);
+            function checkB(){
+                answerOneB.className = 'btn btn-danger m-3 answer-2 w-50';
+                answerOneB.textContent = 'incorrect try again!';
+            }
+
+        let questionOneC = document.querySelector('.option-3');
+            let answerOneC = document.createElement('button');
+                answerOneC.className = "btn btn-success m-3 answer-3 w-50";
+                answerOneC.textContent = "C. Javascript";
+            questionOneC.appendChild(answerOneC);
+            // check answer
+            questionOneC.addEventListener('click', checkC);
+            function checkC (){
+                answerOneC.className = 'btn btn-danger m-3 answer-2 w-50';
+                answerOneC.textContent = 'incorrect try again!';
+            }
+    }
+
+    let secondQuestion = function () {
+        console.log('test');
+        
+    }
+    firstQuestion();
+}
+
+
+ 
+    document.querySelector(".start-button").addEventListener('click', startQuiz);
+
+
