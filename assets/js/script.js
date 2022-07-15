50699379a1ef9d589cafc493c108a674d3b79535
Thomas Ruppeiner <truppeiner@gmail.com>
2022-07-15 14:43:36 -0500

    third question added timer added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,19 @@ let timer = document.querySelector('.timer-clock');
 
 let startQuiz = function() {
     let Questions = function() {
+
+        var timeRemaining = 120;
+
+        var questionTimer = setInterval(function(){
+            if(timeRemaining <= 0){
+                clearInterval(questionTimer);
+                timer.textContent = "No Time Remaining!";
+            }
+            timer.textContent = "Time: " + timeRemaining  + " seconds left";
+    
+            timeRemaining -= 1;
+        }, 1200);
+
         let quizQuestion = document.querySelector(".quiz-question");
             quizQuestion.textContent="What language creates the basic layout of any website?"
             document.querySelector('.start-section').hidden = true;
@@ -25,6 +38,7 @@ let startQuiz = function() {
             function checkB(){
                 answerOneB.className = 'btn btn-danger m-3 answer-2 w-50';
                 answerOneB.textContent = 'incorrect try again!';
+                timeRemaining -= 10;
             }
 
         let questionOneC = document.querySelector('.option-3');
@@ -37,22 +51,47 @@ let startQuiz = function() {
             function checkC (){
                 answerOneC.className = 'btn btn-danger m-3 answer-2 w-50';
                 answerOneC.textContent = 'incorrect try again!';
+                timeRemaining -= 10;
             }
         function secondQuestion(){
             // create second question
             quizQuestion.textContent = "What semantic tag would you use to create a header in HTML?"
-            answerOneA.textContent = '<header>';
+            answerOneA.textContent = 'A. <header>';
             questionOneA.addEventListener('click', thirdQuestion);
 
             answerOneB.className = "btn btn-success m-3 answer-2 w-50";
-            answerOneB.textContent = '<p>';
+            answerOneB.textContent = 'B. <p>';
 
-            answerOneC.className = "btn btn-success m-3 answer-2 w-50";
-            answerOneC.textContent = '<div>';
+            answerOneC.className = "btn btn-success m-3 answer-3 w-50";
+            answerOneC.textContent = 'C. <div>';
         }
 
         function thirdQuestion(){
-            console.log('test');
+            quizQuestion.textContent = "What semantic tag would you use to create a link?"
+            answerOneA.textContent = "A. <a>";
+            questionOneA.addEventListener('click', quizComplete);
+
+            answerOneB.className = "btn btn-success m-3 answer-2 w-50";
+            answerOneB.textContent = "B. <footer>";
+
+            answerOneC.className = "btn btn-success m-3 answer-3 w-50";
+            answerOneC.textContent = "C. <main>";
+        }
+
+        function quizComplete(){
+            quizQuestion.textContent = "Congratulations! You have completed the code quiz!";
+            questionOneA.remove();
+            questionOneB.remove();
+            questionOneC.remove();
+            timer.remove();
+
+            var finalTime = timeRemaining;
+
+            let finalScore = document.createElement('div');
+            finalScore.className = 'final-score m-4'
+            finalScore.textContent = "Your Final Score Is: " + finalTime;
+
+            quizQuestion.appendChild(finalScore);
         }
     }
     Questions();
